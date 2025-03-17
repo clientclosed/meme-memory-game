44a9c8be6b3705bf089ebaed3a5cf30fe587e374
clientclosed <clientclosed@gmail.com>
2025-03-17 16:43:50 +0200

    Add _app.tsx for Next.js application

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Meme Memory Game</title>
+        <meta name="description" content="A fun memory card game featuring popular memes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
