a231472877b9c8b7f4e02b7be10e80d14531e753
clientclosed <clientclosed@gmail.com>
2025-03-17 16:44:12 +0200

    Add meme data

---
+++ data/memes.ts
@@ -0,0 +1,45 @@
+import { MemeData } from '@/types';
+
+// Popular meme images
+export const memes: MemeData[] = [
+  {
+    id: '1',
+    name: 'Distracted Boyfriend',
+    url: 'https://i.imgflip.com/1ur9b0.jpg',
+  },
+  {
+    id: '2',
+    name: 'Drake Hotline Bling',
+    url: 'https://i.imgflip.com/30b1gx.jpg',
+  },
+  {
+    id: '3',
+    name: 'Two Buttons',
+    url: 'https://i.imgflip.com/1g8my4.jpg',
+  },
+  {
+    id: '4',
+    name: 'Change My Mind',
+    url: 'https://i.imgflip.com/24y43o.jpg',
+  },
+  {
+    id: '5',
+    name: 'Expanding Brain',
+    url: 'https://i.imgflip.com/1jwhww.jpg',
+  },
+  {
+    id: '6',
+    name: 'Surprised Pikachu',
+    url: 'https://i.imgflip.com/2kbn1e.jpg',
+  },
+  {
+    id: '7',
+    name: 'Doge',
+    url: 'https://i.imgflip.com/4t0m5.jpg',
+  },
+  {
+    id: '8',
+    name: 'Woman Yelling at Cat',
+    url: 'https://i.imgflip.com/345v97.jpg',
+  },
+];
